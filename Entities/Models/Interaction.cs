47f2566ee33145b9326e87bb6397095976b62fd7
Daniel <ccs2024081@est.univalle.edu>
2023-10-06 22:32:21 -0400

    first commit

---
+++ Entities/Models/Interaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace Entities.Models
+{
+
+    [FirestoreData]
+    public class Interaction : FirebaseDocument
+    {
+        [FirestoreProperty]
+        public string Ip { get; set; }
+
+        [FirestoreProperty]
+        public DateTime Hora { get; set; }
+
+        [FirestoreProperty]
+        public Color TextColor { get; set;}
+
+
+        [FirestoreProperty]
+        public Color ContainerColor { get; set; }
+
+        [FirestoreProperty]
+        public int Reaccion { get; set; }
+    }
+}
---
+++ Entities/Repository/FirestoreRepository.cs
@@ -0,0 +1,43 @@
+﻿using Entities.Models;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Repository
+{
+    public class FirestoreRepository : IFirestoreRepository
+    {
+        private readonly string CollectionName;
+        public FirestoreDb firestoreDb;
+
+        public FirestoreRepository(string collectionName)
+        {
+            string filePath = "/Users/danie/randomcolors-6bf1a-firebase-adminsdk-866ea-5bab870bc1.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filePath);
+            firestoreDb = FirestoreDb.Create(collectionName);
+            this.CollectionName = collectionName;
+        }
+
+
+        public T Get<T>(T record) where T: FirebaseDocument
+        {
+            DocumentReference docRef = firestoreDb.Collection(CollectionName).Document(record.Id);
+            DocumentSnapshot snapshot = docRef.GetSnapshotAsync().GetAwaiter().GetResult();
+
+            if (snapshot.Exists)
+            {
+                T usr = snapshot.ConvertTo<T>();
+                usr.Id = snapshot.Id;
+                return usr;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
